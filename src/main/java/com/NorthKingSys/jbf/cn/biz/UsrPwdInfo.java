2b406ebb61e790d1bc8e0bb3a031c15b2828c6f5
一树一菩提 <7520102+zjj1103@user.noreply.gitee.com>
2020-09-08 16:57:25 +0800

    menu tigui

--- src/main/java/com/NorthKingSys/jbf/cn/biz/UsrPwdInfo.java
+++ src/main/java/com/NorthKingSys/jbf/cn/biz/UsrPwdInfo.java
@@ -13,4 +13,49 @@ public class UsrPwdInfo {
     private String role_name;
     private String password;
     private String role_id;
+    private String sessionId;
+
+    public UsrPwdInfo sessionId(String sessionId) {
+        this.sessionId = sessionId;
+        return this;
+    }
+
+    public UsrPwdInfo user_id(String user_id) {
+        this.user_id = user_id;
+        return this;
+    }
+
+    public UsrPwdInfo user_name(String user_name) {
+        this.user_name = user_name;
+        return this;
+    }
+
+    public UsrPwdInfo create_time(Date create_time) {
+        this.create_time = create_time;
+        return this;
+    }
+
+    public UsrPwdInfo user_role(String user_role) {
+        this.user_role = user_role;
+        return this;
+    }
+
+    public UsrPwdInfo role_name(String role_name) {
+        this.role_name = role_name;
+        return this;
+    }
+
+    public UsrPwdInfo password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UsrPwdInfo role_id(String role_id) {
+        this.role_id = role_id;
+        return this;
+    }
+
+    public static UsrPwdInfo builder(){
+        return new UsrPwdInfo();
+    }
 }
