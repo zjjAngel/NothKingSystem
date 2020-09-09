0afa0b59068f42168e3f899aaf6b0426c22dd17f
一树一菩提 <7520102+zjj1103@user.noreply.gitee.com>
2020-09-09 17:19:56 +0800

    版本问题

--- src/main/java/com/NorthKingSys/jbf/cn/config/InterceptorConfig.java
+++ src/main/java/com/NorthKingSys/jbf/cn/config/InterceptorConfig.java
@@ -1,11 +1,13 @@
 package com.NorthKingSys.jbf.cn.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class InterceptorConfig  extends WebMvcConfigurerAdapter {
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE","OPTIONS" };
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        // 注册拦截器
@@ -23,5 +25,12 @@ public class InterceptorConfig  extends WebMvcConfigurerAdapter {
                  .excludePathPatterns("/registry")
                  .excludePathPatterns("/goto");
 //        super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS)
+                .maxAge(3600);
     }
 }
